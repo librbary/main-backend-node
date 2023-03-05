6c5634aa6097851fb6910f9b9b1c1956e3b64ce2
chandan13tiwari <chandan13tiwaris@gmail.com>
2023-03-05 16:43:57 +0530

    Initial Setup

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+console.log('Hello World!')
+
+var moment = require('moment')
+var date = moment().format('LL')
+console.log(date)
