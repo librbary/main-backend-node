2c21480bdd8b18241fc8c9c09ae1900dbcaa05b9
chandantiwari <chandan13tiwaris@gmail.com>
2023-04-28 12:23:38 +0530

    added netlify deploy

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-module.exports = app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
-
-app.get("/test_url", (req, res, next) => {
-  res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
-});
--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
 const {expect} = require('expect');
 
-const app = require('./app');
+const app = require('./src/app');
 
 describe('Testing sample API', () => {
   it('respond with valid HTTP status code', async () => { // add `async` keyword here
     const response = await request(app)
-      .get('/test_url')
+      .get("/.netlify/functions/app/test")
       .send();
 
     expect(response.status).toBe(200);
