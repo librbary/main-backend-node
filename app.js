58c37685e7e6a3b36a27468f7f819d521f7b5272
chandan13tiwari <chandan13tiwaris@gmail.com>
2023-04-28 09:47:20 +0530

    added test api

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-console.log('Hello World!')
+var express = require("express");
+var app = express();
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
 
-var moment = require('moment')
-var date = moment().format('LL')
-console.log(date)
+
+app.get("/test_url", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+   });
